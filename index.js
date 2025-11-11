1a9f0e224b57b1f47586d9c3267873bff6cb68fd
HasibulHRony <hasibulhrony7@gmail.com>
2025-11-11 09:22:30 +0600

    server set up and connected with mongodb completed.

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+const port = process.env.PORT || 3000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@practicecluster1.opxblvd.mongodb.net/?appName=PracticeCluster1`;
+
+//middleWar
+app.use(cors())
+app.use(express.json())
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db("krishi-link-db")
+        const allProductCollection = db.collection('all_products')
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
